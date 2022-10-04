2b83343f21d3e3421d7b31f8c30289c6ca109174
Rui Lopes <rgl@ruilopes.com>
2022-10-04 18:52:56 +0100

    upgrade to k9s 0.26.6

--- provision-k9s.sh
+++ provision-k9s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-k9s_version="${1:-v0.26.3}"; shift || true
+k9s_version="${1:-v0.26.6}"; shift || true
 
 # download and install.
 wget -qO- "https://github.com/derailed/k9s/releases/download/$k9s_version/k9s_Linux_x86_64.tar.gz" \
