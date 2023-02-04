007874189628c8ca3dc438408ca3d378e790302e
Rui Lopes <rgl@ruilopes.com>
2023-02-04 12:33:07 +0000

    upgrade to k3s 1.26.1+k3s1

--- provision-k3s-server.sh
+++ provision-k3s-server.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 k3s_command="$1"; shift
 k3s_channel="${1:-latest}"; shift
-k3s_version="${1:-v1.26.0+k3s1}"; shift
+k3s_version="${1:-v1.26.1+k3s1}"; shift
 k3s_token="$1"; shift
 flannel_backend="$1"; shift
 ip_address="$1"; shift
@@ -40,7 +40,7 @@ else
 fi
 
 # install k3s.
-# see server arguments at e.g. https://github.com/k3s-io/k3s/blob/v1.26.0+k3s1/pkg/cli/cmds/server.go#L543-L551
+# see server arguments at e.g. https://github.com/k3s-io/k3s/blob/v1.26.1+k3s1/pkg/cli/cmds/server.go#L543-L551
 # or run k3s server --help
 # see https://docs.k3s.io/installation/configuration
 # see https://docs.k3s.io/reference/server-config
@@ -73,7 +73,7 @@ systemctl cat k3s
 k3s check-config || true
 
 # wait for this node to be Ready.
-# e.g. s1     Ready    control-plane,master   3m    v1.26.0+k3s1
+# e.g. s1     Ready    control-plane,master   3m    v1.26.1+k3s1
 $SHELL -c 'node_name=$(hostname); echo "waiting for node $node_name to be ready..."; while [ -z "$(kubectl get nodes $node_name | grep -E "$node_name\s+Ready\s+")" ]; do sleep 3; done; echo "node ready!"'
 
 # wait for the kube-dns pod to be Running.
@@ -87,7 +87,7 @@ $SHELL -c 'while [ -z "$(kubectl get pods --selector k8s-app=kube-dns --namespac
 #       kubectl -n kube-system apply -f /var/lib/rancher/k3s/server/manifests/traefik.yaml
 # see https://doc.traefik.io/traefik/v2.9/operations/api/
 # see https://github.com/k3s-io/k3s/issues/350#issuecomment-511218588
-# see https://github.com/k3s-io/k3s/blob/v1.26.0+k3s1/manifests/traefik.yaml
+# see https://github.com/k3s-io/k3s/blob/v1.26.1+k3s1/manifests/traefik.yaml
 # see https://github.com/traefik/traefik-helm-chart/blob/v20.3.1/traefik/values.yaml
 echo 'configuring traefik...'
 apt-get install -y python3-yaml
@@ -209,7 +209,7 @@ kubectl completion bash >/usr/share/bash-completion/completions/kubectl
 ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
 
 # save kubeconfig in the host.
-# NB the default users are generated at https://github.com/k3s-io/k3s/blob/v1.26.0+k3s1/pkg/daemons/control/deps/deps.go#L224-L251
+# NB the default users are generated at https://github.com/k3s-io/k3s/blob/v1.26.1+k3s1/pkg/daemons/control/deps/deps.go#L224-L251
 #    and saved at /var/lib/rancher/k3s/server/cred/passwd
 mkdir -p /vagrant/tmp
 python3 - <<EOF
