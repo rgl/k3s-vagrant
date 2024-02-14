ac64f2c4e32bff31b2522fff25d030778ca7fc02
Rui Lopes <rgl@ruilopes.com>
2024-02-14 20:11:25 +0000

    upgrade to containerd-shim-spin-v2 0.11.0

--- provision-containerd-shim-spin-v2.sh
+++ provision-containerd-shim-spin-v2.sh
@@ -3,11 +3,11 @@ set -euxo pipefail
 
 # see https://github.com/deislabs/containerd-wasm-shims
 # renovate: datasource=github-releases depName=deislabs/containerd-wasm-shims
-CONTAINERD_WASM_SHIMS_VERSION='0.10.0'
+CONTAINERD_WASM_SHIMS_VERSION='0.11.0'
 
 # bail when already installed.
 if [ -x /usr/local/bin/containerd-shim-spin-v2 ]; then
-    # e.g. Version: 0.3.0
+    # e.g. Version: 0.11.0
     actual_version="$(/usr/local/bin/containerd-shim-spin-v2 -v | perl -ne '/^\s*Version: (.+)/ && print $1')"
     if [ "$actual_version" == "$CONTAINERD_WASM_SHIMS_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
